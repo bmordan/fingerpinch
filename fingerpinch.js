a482d8bc2746b870b5f6c7c0b7064c97096bbee0
Bernard Mordan <bernardmordan@gmail.com>
2014-07-15 21:28:55 +0100

    hammer pinch to zoom

---
+++ fingerpinch.js
@@ -0,0 +1,59 @@
+if (Meteor.isClient) {
+  
+
+  
+  Template.pad.rendered = function(template){
+    var el = this.find('#pad');
+    Hammer(el).on('touch pinch', function(e){
+      //console.log(e);
+      e.gesture.preventDefault();
+      var ox=e.gesture.srcEvent.changedTouches[0].pageX,oy=e.gesture.srcEvent.changedTouches[0].pageY,dx=e.gesture.srcEvent.changedTouches[1].pageX,dy=e.gesture.srcEvent.changedTouches[1].pageY,snap=70;
+      console.log(ox+','+oy+'|'+dx+','+dy);
+      if(ox>dx && oy<dy){
+        var disX=ox-dx;
+        var disY=dy-oy;
+        console.log('/^ '+disX+','+disY);
+        if(disX<snap || disY<snap){disX=0;disY=0;}
+        e.currentTarget.children.markerO.style.left = dx + 'px';
+        e.currentTarget.children.markerO.style.top = oy + 'px';
+        e.currentTarget.children.markerO.style.width = disX + 'px';
+        e.currentTarget.children.markerO.style.height = disY + 'px';
+      }else if(ox<dx && oy<dy){
+        var disX=dx-ox;
+        var disY=dy-oy;
+        console.log('\\^ '+disX+','+disY);
+        if(disX<snap || disY<snap){disX=0;disY=0;}
+        e.currentTarget.children.markerO.style.left = ox + 'px';
+        e.currentTarget.children.markerO.style.top = oy + 'px';
+        e.currentTarget.children.markerO.style.width = disX + 'px';
+        e.currentTarget.children.markerO.style.height = disY + 'px';
+      }else if(ox>dx && oy>dy){
+        var disX=ox-dx;
+        var disY=oy-dy;
+        console.log('\\V '+disX+','+disY);
+        if(disX<snap || disY<snap){disX=0;disY=0;}
+        e.currentTarget.children.markerO.style.left = dx + 'px';
+        e.currentTarget.children.markerO.style.top = dy + 'px';
+        e.currentTarget.children.markerO.style.width = disX + 'px';
+        e.currentTarget.children.markerO.style.height = disY + 'px';
+      }else if(ox<dx && oy>dy){
+        var disX=dx-ox;
+        var disY=oy-dy;
+        console.log('/V '+disX+','+disY);
+        if(disX<snap || disY<snap){disX=0;disY=0;}
+        e.currentTarget.children.markerO.style.left = ox + 'px';
+        e.currentTarget.children.markerO.style.top = dy + 'px';
+        e.currentTarget.children.markerO.style.width = disX + 'px';
+        e.currentTarget.children.markerO.style.height = disY + 'px';      
+      }
+      
+    });
+  }
+}//meteor is no longer client
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+    // sudo rm -rf theInternet <<-- the most dangerous code in the world?
+  });
+}
